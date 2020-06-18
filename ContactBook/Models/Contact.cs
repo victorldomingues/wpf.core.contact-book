7da5c415dfcde11c4054fee82a87ea6980dc0047
Victor Luiz Domingues <victorl.domingues@hotmail.com>
2020-06-18 01:40:56 -0300

    upload

---
+++ ContactBook/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ContactBook.Models
+{
+    public class Contact
+    {
+        public Contact(string name, string phone, string cellPhone, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Phone = phone;
+            CellPhone = cellPhone;
+            Email = email;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Phone { get; private set; }
+        public string CellPhone { get; private set; }
+        public string Email { get; private set; }
+    }
+}
---
+++ ContactBook/ViewModels/ContactBookViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using ContactBook.Models;
+using GalaSoft.MvvmLight;
+
+namespace ContactBook.ViewModels
+{
+    public class ContactBookViewModel : ViewModelBase
+    {
+        public string SearchText { get; set; }
+        public bool SearchInactive { get; set; }
+        public List<Contact> Contacts { get; set; }
+
+    }
+}
